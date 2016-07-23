e87caba55b19a0cab0d0786ef6bb9f55deab82c0
Robert Zhang <robertsky.zhang@gmail.com>
2016-07-23 00:23:32 -0400

    fix mongo client issue

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient, ALL
 
 
-client = MongoClient(os.environ['MONGO_PORT_27017_TCP_ADDR'], 27017)
+client = MongoClient(os.environ['MONGO_PORT_27017_TCP_ADDR'], 27017, connect=False)
 resource = client.main
 admin = client.admin
 
