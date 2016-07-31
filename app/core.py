6ab2547d14f5166960244cae9cff74209ccc088e
Robert Zhang <robertsky.zhang@gmail.com>
2016-07-30 21:41:20 -0400

    single app config

--- app/core.py
+++ app/core.py
@@ -1,7 +1,6 @@
-from flask import Flask, send_from_directory, request, make_response, redirect
+from flask import Flask, send_from_directory
 import logging
 from logging.config import fileConfig
-from jose import jwt, JWTError
 
 app = Flask(__name__, static_folder='./static')
 logging.getLogger('flask_cors').level = logging.DEBUG if app.debug else logging.INFO
@@ -9,34 +8,10 @@ fileConfig('logging_config.ini')
 logger = logging.getLogger()
 
 
-@app.route('/')
-def index():
-    access_token = request.cookies.get('jwt')
-    logger.debug("access_token %s", access_token)
-    if access_token is not None:
-        try:
-            jwt.decode(token=access_token, key=app.secret_key, algorithms='HS256')
-            return make_response(redirect('/dashboard'))
-        except JWTError as e:
-            logger.debug("%s, jwt not verified", str(e))
-            return send_from_directory('template', 'login.html')
-    else:
-        return send_from_directory('template', 'login.html')
-
-
-@app.route('/dashboard')
-def dashboard():
-    access_token = request.cookies.get('jwt')
-    logger.debug("access_token %s", access_token)
-    if access_token is not None:
-        try:
-            logger.debug(jwt.decode(token=access_token, key=app.secret_key, algorithms='HS256'))
-            return send_from_directory('template', 'app.html')
-        except JWTError as e:
-            logger.debug("%s, jwt not verified", str(e))
-            return make_response(redirect('/'))
-    else:
-        return make_response(redirect('/'))
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return send_from_directory('template', 'app.html')
 
 
 @app.after_request
