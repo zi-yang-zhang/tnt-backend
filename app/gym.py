cf6dc8a249a51f6a2610341cad075e75a8949754
Robert Zhang <robertsky.zhang@gmail.com>
2016-12-02 22:27:55 -0500

    updated health

--- app/gym.py
+++ app/gym.py
@@ -15,11 +15,6 @@ from utils import non_empty_str, bearer_header_str
 
 EXPIRY_INFO_TYPE = {"by_count": 1, "by_duration": 2}
 
-# errors = {
-#     'AuthenticationUserNotFound':{
-#
-#     }
-# }
 
 @gym_login_pw_authenticator.get_password
 def verify_pw(email):
